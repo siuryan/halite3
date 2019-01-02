98256c455a22d439439010f4e946645988142879
erichzg <23637791+erichzg@users.noreply.github.com>
2019-01-02 17:11:58 -0500

    adding directional biases to ships

--- util/map_sections.py
+++ util/map_sections.py
@@ -7,15 +7,23 @@ def get_section_values(ship, game_map):
     for i in range(0, 8):
         section_values.append([0, 0, 0, 0, 0, 0, 0, 0])
 
-    for x in range(0, 8):
-        for y in range(0, 8):
-            start_x = x * game_map.width / 8
-            start_y = y * game_map.height / 8
-            end_x = (x + 1) * game_map.width / 8
-            end_y = (y + 1) * game_map.height / 8
+    for x in range(-4, 4):
+        for y in range(-4, 4):
+            start_x = x * game_map.width / 8 + ship.position.x
+            start_y = y * game_map.height / 8 + ship.position.y
+            end_x = (x + 1) * game_map.width / 8 + ship.position.x
+            end_y = (y + 1) * game_map.height / 8 + ship.position.y
             for section_x in range(int(start_x), int(end_x)):
                 for section_y in range(int(start_y), int(end_y)):
-                    section_values[x][y] += 1.0 * (game_map[Position(section_x, section_y)].halite_amount) / (game_map.calculate_distance(ship.position, Position(section_x, section_y)) + 1)
+                    if x > 0 and y > 0 and ship.id % 4 == 0:
+                        section_values[x][y] += 10
+                    elif x < 0 and y > 0 and ship.id % 4 == 1:
+                        section_values[x][y] += 10
+                    elif x < 0 and y < 0 and ship.id % 4 == 2:
+                        section_values[x][y] += 10
+                    elif x > 0 and y < 0 and ship.id % 4 == 3:
+                        section_values[x][y] += 10
+                    section_values[x][y] += 1.0 * (game_map[Position(section_x, section_y)].halite_amount) / (game_map.calculate_distance(ship.position, Position(section_x, section_y)) + 1)**2
     return section_values
 
 def max_dest(section_values, sections_exploring, width, height, shipyard_position):
