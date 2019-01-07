ff27fa9333c8379d492385562dcc90250e7acfb4
erichzg <23637791+erichzg@users.noreply.github.com>
2019-01-07 15:51:16 -0500

    fixed ships crashing

--- MyBot.py
+++ MyBot.py
@@ -122,7 +122,7 @@ while True:
         if len(Ship.next_move_squares[square]) > 1:
             for ship in Ship.next_move_squares[square][1:]:
                 Ship.next_move_squares[square].remove(ship)
-                move = game_map.get_unsafe_moves(ship.position, nav.collect_halite(game_map, ship))[0]
+                move = game_map.naivest_navigate(ship.position, nav.collect_halite(game_map, ship))
                 command_queue.append(ship.move(move))
 
     # If you're on the first turn and have enough halite, spawn a ship.
